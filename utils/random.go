a87419ea6a6ce47bbbb8a284f6a981eebf53baf9
William Gomez <williamemir@gmail.com>
2022-11-08 00:43:08 -0500

    Fix random string generator

--- utils/random.go
+++ utils/random.go
@@ -2,14 +2,26 @@ package utils
 
 import (
 	"math/rand"
+	"time"
 )
 
 const alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func RandomString(n int) string {
+func RandomString_old(n int) string {
 	rndString := make([]byte, n)
 	for i := range rndString {
 		rndString[i] = alphabet[rand.Int63() % int64(len(alphabet))]
 	}
 	return  string(rndString)
+}
+
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func RandomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = alphabet[seededRand.Intn(len(alphabet))]
+	}
+	return string(b)
 }
